df6ba7ef49bd1171d9a82d10a02589bba2761f4f
langtumuathufa <77397954+langtumuathufa@users.noreply.github.com>
2023-02-15 15:12:13 +0700

    Update Cau7.dart

--- Cau7.dart
+++ Cau7.dart
@@ -1,15 +1,13 @@
 import 'dart:io';
 
 void main() {
-  // Nhập số từ người dùng
-  stdout.write("Nhập một số: ");
-  int num = int.parse(stdin.readLineSync()!);
+  stdout.write("first: ");
+  int first = int.parse(stdin.readLineSync()!);
+  stdout.write("second: ");
+  int second = int.parse(stdin.readLineSync()!);
 
-  // In hình vuông
-  for (int i = 1; i <= num; i++) {
-    for (int j = 1; j <= num; j++) {
-      stdout.write("* ");
-    }
-    stdout.write("\n");
-  }
+  int quotient = first ~/ second;
+  int remainder = first % second;
+
+  print("quotient of $first and $second is $quotient, with a remainder of $remainder.");
 }
